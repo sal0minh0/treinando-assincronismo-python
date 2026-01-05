217c88c6b7dc96f9d850e825a38caaaf92e9f0db
sal0minh0 <salomaomoraes1@hotmail.com>
2026-01-05 17:11:03 -0300

    docs: adicionar comentarios de ordem de tempo de respota

--- consumo-pokemon-api.py
+++ consumo-pokemon-api.py
@@ -21,8 +21,8 @@ async def main():
     """Função main para colocar nossos pokémons"""
     async with httpx.AsyncClient() as cliente:
         #await fetch_get(cliente, "squirtle") # Atuação Síncrona
-        await asyncio.gather(
-            fetch_get(cliente, "squirtle"),
+        await asyncio.gather( # Isso aqui vai ser Assíncrono, mas a ordem dos dados depende do tempo de resposta do cliente
+            fetch_get(cliente, "squirtle"), # Tem como vim em ordem como você também, aí é de outro jeito
             fetch_get(cliente, "charmander" ),
             fetch_get(cliente, "bulbasaur")
         )
